cd6eea3a8586038e6c41f2ccbce89794c183afc5
blumerceline <blumerceline@gmail.com>
2020-02-27 21:43:55 +1000

    Updates

---
+++ precache-manifest.7392ca6cb400f7b2690277e6e73d630f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23fa0c07ebbbc6db472d22def756b454",
+    "url": "/StudyLamp/index.html"
+  },
+  {
+    "revision": "eb751eed2a4971914e31",
+    "url": "/StudyLamp/static/css/main.b854dbf9.chunk.css"
+  },
+  {
+    "revision": "cb4a8d2f5a3ad7a0dbd1",
+    "url": "/StudyLamp/static/js/2.9608af71.chunk.js"
+  },
+  {
+    "revision": "eb751eed2a4971914e31",
+    "url": "/StudyLamp/static/js/main.4d7fbafa.chunk.js"
+  },
+  {
+    "revision": "38604ea744886698b355",
+    "url": "/StudyLamp/static/js/runtime~main.6120248d.js"
+  }
+]);
